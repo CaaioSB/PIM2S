023cef75812ee536280ebd6cdb692e6f3b935303
Caio Silva <caio_silvabatista@hotmail.com>
2020-05-05 13:37:31 -0300

    Adicionando o projeto uiCSB na solução

---
+++ uiCSB/uiButton.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace uiCSB
+{
+    public class uiButton : Button
+    {
+        private Color _borderColor = Color.Silver;
+        private Color _onHoverBorderColor = Color.Gray;
+        private Color _buttonColor = Color.Red;
+        private Color _onHoverButtonColor = Color.Yellow;
+        private Color _textColor = Color.White;
+        private Color _onHoverTextColor = Color.Gray;
+
+        private bool _isHovering;
+
+        public uiButton()
+        {
+            FlatAppearance.BorderSize = 0;
+            FlatAppearance.MouseOverBackColor = Color.Transparent;
+            FlatAppearance.MouseDownBackColor = Color.Transparent;
+            FlatStyle = FlatStyle.Flat;
+
+            DoubleBuffered = true;
+            MouseEnter += (sender, e) =>
+            {
+                _isHovering = true;
+                Invalidate();
+            };
+            MouseLeave += (sender, e) =>
+            {
+                _isHovering = false;
+                Invalidate();
+            };
+        }
+
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics g = e.Graphics;
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            Brush brush = new SolidBrush(_isHovering ? _onHoverBorderColor : _borderColor);
+
+            //Border
+            //g.FillEllipse(brush, 0, 0, Height, Height);
+            //g.FillEllipse(brush, Width - Height, 0, Height, Height);
+            //g.FillRectangle(brush, Height / 2, 0, Width - Height, Height);
+
+            brush.Dispose();
+            brush = new SolidBrush(_isHovering ? _onHoverButtonColor : _buttonColor);
+
+            //Inner part. Button itself
+            //g.FillEllipse(brush, _borderThicknessByTwo, _borderThicknessByTwo, Height - _borderThickness,
+            //    Height - _borderThickness);
+            //g.FillEllipse(brush, (Width - Height) + _borderThicknessByTwo, _borderThicknessByTwo,
+            //    Height - _borderThickness, Height - _borderThickness);
+
+            RectangleF myRectangleF = new RectangleF(0F, 0F, Width - 3, Height - 3);
+
+            Rectangle roundedRectangle = Rectangle.Round(myRectangleF);
+
+            // Draw the rounded rectangle in red.
+            Pen redPen = new Pen(brush, 1);
+            //e.Graphics.DrawRectangle(redPen, roundedRectangle);
+            RoundedRectangle.FillRoundedRectangle(e.Graphics, brush, roundedRectangle, 5);
+            //e.Graphics.DrawPath(redPen, );
+
+            //g.FillRectangle(brush, Height / 2 + _borderThicknessByTwo, _borderThicknessByTwo,
+            //    Width - Height - _borderThickness, Height - _borderThickness);
+
+            brush.Dispose();
+            brush = new SolidBrush(_isHovering ? _onHoverTextColor : _textColor);
+
+            //FontFamily fontFamily = new FontFamily("Sofia Pro Light");
+
+            //Font font = new Font(
+            //    fontFamily,
+            //    15,
+            //    FontStyle.Regular,
+            //    GraphicsUnit.Pixel
+            //    );
+
+
+            //Button Text
+            SizeF stringSize = g.MeasureString(Text, Font);
+            g.DrawString(Text, Font, brush, ((Width) - stringSize.Width) / 2, ((Height) - stringSize.Height) / 2);
+        }
+
+        public Color BorderColor
+        {
+            get => _borderColor;
+            set
+            {
+                _borderColor = value;
+                Invalidate();
+            }
+        }
+
+        public Color OnHoverBorderColor
+        {
+            get => _onHoverBorderColor;
+            set
+            {
+                _onHoverBorderColor = value;
+                Invalidate();
+            }
+        }
+
+        public Color ButtonColor
+        {
+            get => _buttonColor;
+            set
+            {
+                _buttonColor = value;
+                Invalidate();
+            }
+        }
+
+        public Color OnHoverButtonColor
+        {
+            get => _onHoverButtonColor;
+            set
+            {
+                _onHoverButtonColor = value;
+                Invalidate();
+            }
+        }
+
+        public Color TextColor
+        {
+            get => _textColor;
+            set
+            {
+                _textColor = value;
+                Invalidate();
+            }
+        }
+
+        public Color OnHoverTextColor
+        {
+            get => _onHoverTextColor;
+            set
+            {
+                _onHoverTextColor = value;
+                Invalidate();
+            }
+        }
+    }
+
+    static class RoundedRectangle
+    {
+        public static void FillRoundedRectangle(this Graphics graphics, Brush brush, Rectangle bounds, int cornerRadius)
+        {
+            if (graphics == null)
+                throw new ArgumentNullException("graphics");
+            if (brush == null)
+                throw new ArgumentNullException("brush");
+
+            using (GraphicsPath path = RoundedRect(bounds, cornerRadius))
+            {
+                graphics.FillPath(brush, path);
+            }
+        }
+
+        public static GraphicsPath RoundedRect(Rectangle bounds, int radius)
+        {
+            int diameter = radius * 2;
+            Size size = new Size(diameter, diameter);
+            Rectangle arc = new Rectangle(bounds.Location, size);
+            GraphicsPath path = new GraphicsPath();
+
+            if (radius == 0)
+            {
+                path.AddRectangle(bounds);
+                return path;
+            }
+
+            // top left arc  
+            path.AddArc(arc, 180, 90);
+
+            // top right arc  
+            arc.X = bounds.Right - diameter;
+            path.AddArc(arc, 270, 90);
+
+            // bottom right arc  
+            arc.Y = bounds.Bottom - diameter;
+            path.AddArc(arc, 0, 90);
+
+            // bottom left arc 
+            arc.X = bounds.Left;
+            path.AddArc(arc, 90, 90);
+
+            path.CloseFigure();
+            return path;
+        }
+
+    }
+}
---
+++ uiCSB/uiPanel.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uiCSB
+{
+    public class uiPanel : Panel
+    {
+        public uiPanel()
+        {
+            DoubleBuffered = true;
+            //MouseEnter += (sender, e) =>
+            //{
+            //    _isHovering = true;
+            //    Invalidate();
+            //};
+            //MouseLeave += (sender, e) =>
+            //{
+            //    _isHovering = false;
+            //    Invalidate();
+            //};
+        }
+
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics g = e.Graphics;
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            Brush brush = new SolidBrush(Color.FromArgb(242, 245, 255));
+            
+            //Border
+            //g.FillEllipse(brush, 0, 0, Height, Height);
+            //g.FillEllipse(brush, Width - Height, 0, Height, Height);
+            //g.FillRectangle(brush, Height / 2, 0, Width - Height, Height);
+
+            //brush.Dispose();
+            //brush = new SolidBrush(_isHovering ? _onHoverButtonColor : _buttonColor);
+
+            //Inner part. Button itself
+            //g.FillEllipse(brush, _borderThicknessByTwo, _borderThicknessByTwo, Height - _borderThickness,
+            //    Height - _borderThickness);
+            //g.FillEllipse(brush, (Width - Height) + _borderThicknessByTwo, _borderThicknessByTwo,
+            //    Height - _borderThickness, Height - _borderThickness);
+
+            RectangleF myRectangleF = new RectangleF(0F, 0F, Width - 3, Height - 3);
+
+            Rectangle roundedRectangle = Rectangle.Round(myRectangleF);
+
+            // Draw the rounded rectangle in red.
+            Pen redPen = new Pen(brush, 1);
+            //e.Graphics.DrawRectangle(redPen, roundedRectangle);
+            RoundedRectangle.FillRoundedRectangle(e.Graphics, brush, roundedRectangle, 5);
+            //e.Graphics.DrawPath(redPen, );
+
+            //g.FillRectangle(brush, Height / 2 + _borderThicknessByTwo, _borderThicknessByTwo,
+            //    Width - Height - _borderThickness, Height - _borderThickness);
+
+            brush.Dispose();
+            //brush = new SolidBrush(_isHovering ? _onHoverTextColor : _textColor);
+
+            //FontFamily fontFamily = new FontFamily("Sofia Pro Light");
+
+            //Font font = new Font(
+            //    fontFamily,
+            //    15,
+            //    FontStyle.Regular,
+            //    GraphicsUnit.Pixel
+            //    );
+
+
+            //Button Text
+            //SizeF stringSize = g.MeasureString(Text, Font);
+            //g.DrawString(Text, Font, brush, ((Width) - stringSize.Width) / 2, ((Height) - stringSize.Height) / 2);
+        }
+    }
+}
