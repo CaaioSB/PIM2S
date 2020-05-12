b2fe3d584b98a6ef00e0adc2571c464b98ddac5f
Caio Silva <caio_silvabatista@hotmail.com>
2020-05-12 18:50:05 -0300

    Alterações automáticas

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Blockchain.Formularios
+namespace Etherchain.Formularios
 {
     static class Program
     {
@@ -16,7 +16,7 @@ namespace Blockchain.Formularios
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmAcesso());
+            Application.Run(new frmBase());
         }
     }
 }
