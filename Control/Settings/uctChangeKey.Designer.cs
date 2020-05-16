8ca04f63205dfc34566bb9732f49172067e1fc03
Caio Silva <caio_silvabatista@hotmail.com>
2020-05-16 02:49:38 -0300

    Criação do controle de troca de senha

---
+++ Control/Settings/uctChangeKey.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Etherchain.Control.Settings
+{
+    partial class uctChangeKey
+    {
+        /// <summary> 
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Designer de Componentes
+
+        /// <summary> 
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // uctChangeKey
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(235)))), ((int)(((byte)(255)))));
+            this.Name = "uctChangeKey";
+            this.Size = new System.Drawing.Size(822, 320);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
