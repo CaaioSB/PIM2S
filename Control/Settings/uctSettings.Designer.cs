0878ae41b7af7d68c356b051b021453273e8846f
Caio Silva <caio_silvabatista@hotmail.com>
2020-05-16 02:48:45 -0300

    Protótipo do controle "Settings" concluído

---
+++ Control/Settings/uctSettings.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace Etherchain.Control
+{
+    partial class uctSettings
+    {
+        /// <summary> 
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Designer de Componentes
+
+        /// <summary> 
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uiFlowPanel1 = new uiCSB.Component.uiFlowPanel();
+            this.uiBtnAcessibilidade = new uiCSB.Component.uiButton();
+            this.uiButton1 = new uiCSB.Component.uiButton();
+            this.uiPanelControl = new uiCSB.Component.uiPanel();
+            this.lblDescControl = new System.Windows.Forms.Label();
+            this.lblTextControl = new System.Windows.Forms.Label();
+            this.picImageControl = new System.Windows.Forms.PictureBox();
+            this.uiPnlConteudo = new uiCSB.Component.uiPanel();
+            this.uctChangeKey = new Etherchain.Control.Settings.uctChangeKey();
+            this.uctAcessibilidade = new Etherchain.Control.uctAcessibilidade();
+            this.uiFlowPanel1.SuspendLayout();
+            this.uiPanelControl.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picImageControl)).BeginInit();
+            this.uiPnlConteudo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // uiFlowPanel1
+            // 
+            this.uiFlowPanel1.Controls.Add(this.uiBtnAcessibilidade);
+            this.uiFlowPanel1.Controls.Add(this.uiButton1);
+            this.uiFlowPanel1.FlowPanelColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(235)))), ((int)(((byte)(255)))));
+            this.uiFlowPanel1.Location = new System.Drawing.Point(0, 143);
+            this.uiFlowPanel1.Name = "uiFlowPanel1";
+            this.uiFlowPanel1.Size = new System.Drawing.Size(834, 90);
+            this.uiFlowPanel1.TabIndex = 9;
+            // 
+            // uiBtnAcessibilidade
+            // 
+            this.uiBtnAcessibilidade.BackColor = System.Drawing.Color.Transparent;
+            this.uiBtnAcessibilidade.BorderColor = System.Drawing.Color.Transparent;
+            this.uiBtnAcessibilidade.ButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(72)))), ((int)(((byte)(84)))), ((int)(((byte)(179)))));
+            this.uiBtnAcessibilidade.FlatAppearance.BorderSize = 0;
+            this.uiBtnAcessibilidade.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.uiBtnAcessibilidade.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.uiBtnAcessibilidade.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.uiBtnAcessibilidade.Location = new System.Drawing.Point(3, 3);
+            this.uiBtnAcessibilidade.Name = "uiBtnAcessibilidade";
+            this.uiBtnAcessibilidade.OnHoverBorderColor = System.Drawing.Color.Transparent;
+            this.uiBtnAcessibilidade.OnHoverButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(72)))), ((int)(((byte)(65)))), ((int)(((byte)(179)))));
+            this.uiBtnAcessibilidade.OnHoverTextColor = System.Drawing.Color.White;
+            this.uiBtnAcessibilidade.Size = new System.Drawing.Size(147, 84);
+            this.uiBtnAcessibilidade.TabIndex = 8;
+            this.uiBtnAcessibilidade.Text = "ACESSIBILIDADE";
+            this.uiBtnAcessibilidade.TextColor = System.Drawing.Color.White;
+            this.uiBtnAcessibilidade.UseVisualStyleBackColor = false;
+            this.uiBtnAcessibilidade.Click += new System.EventHandler(this.uiBtnAcessibilidade_Click);
+            // 
+            // uiButton1
+            // 
+            this.uiButton1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(235)))), ((int)(((byte)(255)))));
+            this.uiButton1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(235)))), ((int)(((byte)(255)))));
+            this.uiButton1.ButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.uiButton1.FlatAppearance.BorderSize = 0;
+            this.uiButton1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.uiButton1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.uiButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.uiButton1.Location = new System.Drawing.Point(156, 3);
+            this.uiButton1.Name = "uiButton1";
+            this.uiButton1.OnHoverBorderColor = System.Drawing.Color.White;
+            this.uiButton1.OnHoverButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(96)))), ((int)(((byte)(0)))));
+            this.uiButton1.OnHoverTextColor = System.Drawing.Color.White;
+            this.uiButton1.Size = new System.Drawing.Size(147, 84);
+            this.uiButton1.TabIndex = 7;
+            this.uiButton1.Text = "ALTERAR SENHA";
+            this.uiButton1.TextColor = System.Drawing.Color.White;
+            this.uiButton1.UseVisualStyleBackColor = false;
+            this.uiButton1.Click += new System.EventHandler(this.uiButton1_Click);
+            // 
+            // uiPanelControl
+            // 
+            this.uiPanelControl.Controls.Add(this.lblDescControl);
+            this.uiPanelControl.Controls.Add(this.lblTextControl);
+            this.uiPanelControl.Controls.Add(this.picImageControl);
+            this.uiPanelControl.Location = new System.Drawing.Point(0, 0);
+            this.uiPanelControl.Name = "uiPanelControl";
+            this.uiPanelControl.PanelColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(245)))), ((int)(((byte)(255)))));
+            this.uiPanelControl.PanelRadius = 5;
+            this.uiPanelControl.Size = new System.Drawing.Size(834, 125);
+            this.uiPanelControl.TabIndex = 6;
+            // 
+            // lblDescControl
+            // 
+            this.lblDescControl.AutoSize = true;
+            this.lblDescControl.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(245)))), ((int)(((byte)(255)))));
+            this.lblDescControl.Font = new System.Drawing.Font("Gadugi", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescControl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(182)))), ((int)(((byte)(194)))));
+            this.lblDescControl.Location = new System.Drawing.Point(143, 50);
+            this.lblDescControl.Name = "lblDescControl";
+            this.lblDescControl.Size = new System.Drawing.Size(491, 19);
+            this.lblDescControl.TabIndex = 6;
+            this.lblDescControl.Text = "Realize pequenos ajustes aqui, e deixe o Etherchain com a sua cara :D";
+            // 
+            // lblTextControl
+            // 
+            this.lblTextControl.AutoSize = true;
+            this.lblTextControl.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(245)))), ((int)(((byte)(255)))));
+            this.lblTextControl.Font = new System.Drawing.Font("Gadugi", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTextControl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(63)))), ((int)(((byte)(153)))));
+            this.lblTextControl.Location = new System.Drawing.Point(140, 11);
+            this.lblTextControl.Name = "lblTextControl";
+            this.lblTextControl.Size = new System.Drawing.Size(213, 35);
+            this.lblTextControl.TabIndex = 5;
+            this.lblTextControl.Text = "Configurações";
+            // 
+            // picImageControl
+            // 
+            this.picImageControl.BackColor = System.Drawing.Color.Transparent;
+            this.picImageControl.Image = global::Etherchain.Properties.Resources.support;
+            this.picImageControl.Location = new System.Drawing.Point(15, 11);
+            this.picImageControl.Name = "picImageControl";
+            this.picImageControl.Size = new System.Drawing.Size(119, 103);
+            this.picImageControl.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picImageControl.TabIndex = 4;
+            this.picImageControl.TabStop = false;
+            // 
+            // uiPnlConteudo
+            // 
+            this.uiPnlConteudo.Controls.Add(this.uctChangeKey);
+            this.uiPnlConteudo.Controls.Add(this.uctAcessibilidade);
+            this.uiPnlConteudo.Location = new System.Drawing.Point(0, 239);
+            this.uiPnlConteudo.Name = "uiPnlConteudo";
+            this.uiPnlConteudo.PanelColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(235)))), ((int)(((byte)(255)))));
+            this.uiPnlConteudo.PanelRadius = 5;
+            this.uiPnlConteudo.Size = new System.Drawing.Size(834, 329);
+            this.uiPnlConteudo.TabIndex = 11;
+            // 
+            // uctChangeKey
+            // 
+            this.uctChangeKey.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(235)))), ((int)(((byte)(255)))));
+            this.uctChangeKey.Location = new System.Drawing.Point(359, 195);
+            this.uctChangeKey.Name = "uctChangeKey";
+            this.uctChangeKey.Size = new System.Drawing.Size(822, 320);
+            this.uctChangeKey.TabIndex = 11;
+            // 
+            // uctAcessibilidade
+            // 
+            this.uctAcessibilidade.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(235)))), ((int)(((byte)(255)))));
+            this.uctAcessibilidade.Location = new System.Drawing.Point(35, 30);
+            this.uctAcessibilidade.Name = "uctAcessibilidade";
+            this.uctAcessibilidade.Size = new System.Drawing.Size(822, 320);
+            this.uctAcessibilidade.TabIndex = 10;
+            this.uctAcessibilidade.Visible = false;
+            // 
+            // uctSettings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.uiPnlConteudo);
+            this.Controls.Add(this.uiFlowPanel1);
+            this.Controls.Add(this.uiPanelControl);
+            this.Name = "uctSettings";
+            this.Size = new System.Drawing.Size(834, 568);
+            this.uiFlowPanel1.ResumeLayout(false);
+            this.uiPanelControl.ResumeLayout(false);
+            this.uiPanelControl.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picImageControl)).EndInit();
+            this.uiPnlConteudo.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private uiCSB.Component.uiPanel uiPanelControl;
+        private System.Windows.Forms.Label lblDescControl;
+        private System.Windows.Forms.Label lblTextControl;
+        private System.Windows.Forms.PictureBox picImageControl;
+        private uiCSB.Component.uiButton uiButton1;
+        private uiCSB.Component.uiFlowPanel uiFlowPanel1;
+        private uiCSB.Component.uiButton uiBtnAcessibilidade;
+        private uctAcessibilidade uctAcessibilidade;
+        private uiCSB.Component.uiPanel uiPnlConteudo;
+        private Settings.uctChangeKey uctChangeKey;
+    }
+}
---
+++ Control/Settings/uctSettings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Etherchain.Control
+{
+    public partial class uctSettings : UserControl
+    {
+        public uctSettings()
+        {
+            InitializeComponent();
+        }
+
+        private void uiBtnAcessibilidade_Click(object sender, EventArgs e)
+        {
+            Util.Util.MudarConteudo(uiPnlConteudo, uctAcessibilidade);
+        }
+
+        private void uiButton1_Click(object sender, EventArgs e)
+        {
+            Util.Util.MudarConteudo(uiPnlConteudo, uctChangeKey);
+        }
+    }
+}
