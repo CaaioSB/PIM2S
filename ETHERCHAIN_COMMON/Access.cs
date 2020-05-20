2573409da9226b884a47d5280df3b5a18dfd4583
Caio Silva <caio_silvabatista@hotmail.com>
2020-05-20 15:21:03 -0300

    Novos atributos e SQLMaster integrado

--- ETHERCHAIN_COMMON/Access.cs
+++ ETHERCHAIN_COMMON/Access.cs
@@ -1,33 +1,70 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using caiosb.SqlMaster.Command;
+using caiosb.Util.Data;
 
-namespace ETHERCHAIN_COMMON
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace Etherchain.Common
 {
     public class Access
     {
         private int _accessId;
+        private int _employeeId;
         private string _cpf;
         private string _hash;
-        private string _accessLevel;
+        private int _accessLevel;
         private bool _active;
 
         public int AccessId { get => _accessId; set => _accessId = value; }
         public string Cpf { get => _cpf; set => _cpf = value; }
         public string Hash { get => _hash; set => _hash = value; }
-        public string AccessLevel { get => _accessLevel; set => _accessLevel = value; }
+        public int AccessLevel { get => _accessLevel; set => _accessLevel = value; }
         public bool Active { get => _active; set => _active = value; }
+        public int EmployeeId { get => _employeeId; set => _employeeId = value; }
 
-        public int? AutenticarPorUsuario()
+        private Access obter(SqlDataReader reader) => new Access
         {
-            return 0;
+            AccessId = UtilConvert.ToInt(reader["ACCESS_ID"]),
+            EmployeeId = UtilConvert.ToInt(reader["EMPLOYEE_ID"]),
+            Cpf = UtilConvert.ToString(reader["CPF"]),
+            Hash = UtilConvert.ToString(reader["HASH"]),
+            AccessLevel = UtilConvert.ToInt(reader["ACCESS_LEVEL"]),
+            Active = UtilConvert.ToBool(reader["ACTIVE"])
+        };
+
+        public int? AutenticarPorCPF()
+        {
+            sqlSelect objSelect = new sqlSelect();
+            objSelect.table("Access");
+            objSelect.Columns.select("EMPLOYEE_ID");
+            objSelect.Where.where("CPF", caiosb.SqlMaster.Element.Where.sqlElementWhereCommon.whereOperation.EQUALS, Cpf)
+                           .where("HASH", caiosb.SqlMaster.Element.Where.sqlElementWhereCommon.whereOperation.EQUALS, Hash)
+                           .where("ACTIVE", UtilConvert.ToBit(Active));
+
+            int? codFuncionario = null;
+            SqlDataReader reader = objSelect.execute(App.DatabaseSql);
+            if (reader.Read())
+                codFuncionario = UtilConvert.ToInt(reader["EMPLOYEE_ID"]);
+            reader.Close();
+
+            return codFuncionario;
         }
 
-        public string AutenticarPorCPF()
+        public Access ObterPorId()
         {
-            return String.Empty;
+            sqlSelect objSelect = new sqlSelect();
+            objSelect.table("Access");
+            objSelect.Where.where("EMPLOYEE_ID", EmployeeId);
+
+            SqlDataReader reader = objSelect.execute(App.DatabaseSql);
+            Access access = null;
+            while (reader.Read())
+                access = obter(reader);
+            reader.Close();
+
+            return access;
+
         }
 
         public Access ObterPorCPF()
@@ -44,7 +81,16 @@ namespace ETHERCHAIN_COMMON
 
         public List<Access> ObterTodos()
         {
-            return new List<Access>();
+            sqlSelect objSelect = new sqlSelect();
+            objSelect.table("Access");
+
+            List<Access> objAcessos = new List<Access>();
+            SqlDataReader reader = objSelect.execute(App.DatabaseSql);
+            while (reader.Read())
+                objAcessos.Add(obter(reader));
+            reader.Close();
+
+            return objAcessos;
         }
 
         public bool Gravar()
