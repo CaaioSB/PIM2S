39272405b9bed3b6a56abf2705ad73f1e02d6349
Caio Silva <caio_silvabatista@hotmail.com>
2020-06-16 16:12:48 -0300

    Armazenamento da foto do funcionário na memoria enquanto logado.

--- ETHERCHAIN_DESKTOP/AppDesktop.cs
+++ ETHERCHAIN_DESKTOP/AppDesktop.cs
@@ -10,8 +10,11 @@ namespace Etherchain.Desktop
     class AppDesktop
     {
         private static Employee _actualEmployee;
+        private static Archive _actualArchive;
 
         internal static Employee ActualEmployee { get => _actualEmployee; set => _actualEmployee = value; }
+
+        internal static Archive ActualArchive { get => _actualArchive; set => _actualArchive = value; }
 
         internal static void startup()
         {
