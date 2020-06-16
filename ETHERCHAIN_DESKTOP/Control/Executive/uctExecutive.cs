435c66b53c6c04fafa6624320665982bbb7f8106
Caio Silva <caio_silvabatista@hotmail.com>
2020-06-16 16:11:08 -0300

    Adicionado evento click ao botão "Acesso".

--- ETHERCHAIN_DESKTOP/Control/Executive/uctExecutive.cs
+++ ETHERCHAIN_DESKTOP/Control/Executive/uctExecutive.cs
@@ -24,5 +24,10 @@ namespace Etherchain.Control.Executive
         {
             UtilChangePanel.MudarConteudo(uiPanelConteudo, new uctFuncionario());
         }
+
+        private void uiBtnAcesso_Click(object sender, EventArgs e)
+        {
+            UtilChangePanel.MudarConteudo(uiPanelConteudo, new uctAcesso());
+        }
     }
 }
