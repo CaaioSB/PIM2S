bfe5cd4effaaeb2e60125fc114059eb9b210b7f6
Caio Silva <caio_silvabatista@hotmail.com>
2020-06-16 16:21:06 -0300

    Classe responsável por obter os possíveis cargos dos funcionários.

---
+++ ETHERCHAIN_COMMON/Position.cs
@@ -0,0 +1,40 @@
+﻿using caiosb.SqlMaster.Command;
+using caiosb.Util.Data;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Etherchain.Common
+{
+    public class Position
+    {
+        private int _positionId;
+        private string _name;
+
+        public int PositionId { get => _positionId; set => _positionId = value; }
+        public string Name { get => _name; set => _name = value; }
+
+        private Position obter(SqlDataReader reader) => new Position
+        {
+            PositionId = UtilConvert.ToInt(reader["ID"]),
+            Name = UtilConvert.ToString(reader["NAME"]),
+        };
+
+        public List<Position> ObterTodos()
+        {
+            sqlSelect objSelect = new sqlSelect();
+            objSelect.table("Position");
+
+            List<Position> objAcessos = new List<Position>();
+            SqlDataReader reader = objSelect.execute(App.DatabaseSql);
+            while (reader.Read())
+                objAcessos.Add(obter(reader));
+            reader.Close();
+
+            return objAcessos;
+        }
+    }
+}
