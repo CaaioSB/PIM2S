c2de5a119cd8ef9034d8ef83e67663b0fdd8b708
Caio Silva <caio_silvabatista@hotmail.com>
2020-06-16 16:02:46 -0300

    Classe para conversão de imagem (imagem para byte e reverso).

---
+++ UTIL/UtilImage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caiosb.Util
+{
+    public static class UtilImage
+    {
+        public static byte[] ImageToByte(Image img)
+        {
+            ImageConverter converter = new ImageConverter();
+            return (byte[])converter.ConvertTo(img, typeof(byte[]));
+        }
+
+        public static Image ByteToImage(byte[] byteImg)
+        {
+            using (MemoryStream memstr = new MemoryStream(byteImg))
+            {
+                Image img = Image.FromStream(memstr);
+                return img;
+            }
+        }
+    }
+}
